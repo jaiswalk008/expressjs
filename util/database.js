8208a19bebf04f8dd7e8979cfdcf1b53292e89ac
jaiswalk008 <jaiskaran008@gmail.com>
2023-06-22 16:57:44 +0530

    connected sequelize

--- util/database.js
+++ util/database.js
@@ -1,21 +1,8 @@
-
-const mysql = require('mysql2');
-/*
-    Connection object - It is used to run queries
-    There are two ways of connecting to a sql database
-    1.We setup one connection to run queries and then close the connection after running the query
-    The downside is that we need to again execute the code to setup a new connection for 
-    a new query.So it will be very inefficient.
-    
-    2. Better way = connection pool-> takes js object to provide info about the database
-*/
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'karan123'
-})
-
-//we can use a pool to get a new connection for new queries to run. pools finishes 
-//after the code is executed.
-module.exports = pool.promise();//to handle queries asynchronously
+//sequelize uses mysql
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete','root','karan123',
+{dialect:'mysql',
+host:'localhost',//chooses localhost by default
+});
+//creates a connection pool automatically and connects to the database
+module.exports = sequelize;
