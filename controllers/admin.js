35649e6a4c03ff3df7a9fd3744411d01fc533d85
jaiswalk008 <jaiskaran008@gmail.com>
2023-06-27 20:49:54 +0530

    defined relationship b/w user and product

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,12 +14,23 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(err => console.log(err));
+  //create- its  creates a new element based on the model and saves it in the database
+  //build - build creates a new element in javascript and we have to save it manually
+  //sequelize works with promises
+
+  //since req.user is a sequelize object so...
+  //createProduct because of association and because of that 
+  //we dont have to add usrId explicitly
+  req.user.createProduct({
+    title:title,
+    price:price,
+    imageUrl:imageUrl,
+    description:description
+  }).then(() => {
+    console.log('product created');
+    res.redirect('/admin/products');
+  })
+  .catch(err=> console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -28,17 +39,19 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
+  //products connected to a partidcular user
+  req.user.getProducts({where: { id : prodId}})
+  //will return a product array
+  //Product.findByPk(prodId)
+  .then(product => {
+    if (!product) return res.redirect('/');
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: product
+      product: product[0]
     });
-  });
+  }).catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -47,33 +60,55 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+
+  Product.findByPk(prodId)
+  .then((product)=>{
+    product.title = updatedTitle,
+    product.price= updatedPrice,
+    product.description = updatedDesc,
+    product.imageUrl = updatedImageUrl
+    //the changes to product till is now is local and save method
+    //will make the changes in the database
+    return product.save();
+  })
+  //the below then is handling the promise object from the return 
+  // product.save statement and errors from both the promises 
+  //will be handled by the catch statement below
+  .then(result => {
+    console.log('updated product');
+    res.redirect('/admin/products');
+})
+  //changes will be only visible after refrshing because
+  //the below code is synchronous and promises are aysnchrnous
+  // res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('admin/products', {
-        prods: rows,
-        pageTitle: 'Admin Products',
-        path: '/admin/products'
-      });
-    })
-    .catch(err => console.log(err));
+  //getting all products for a particular user
+  req.user
+  .getProducts()
+  //Product.findAll().
+  .then((products =>{
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  })).catch(err => console.log(err));
+    
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-  .then(()=>{
+  //Product.destroy({});giving a wher condition
+  Product.findByPk(prodId)
+  .then(product =>{
+    return product.destroy();
+  })
+  .then(result =>{
+    console.log('deleted product');
     res.redirect('/admin/products');
-  }).catch(err => console.log(err))
+  })
+  .catch(err=>console.log(err))
+  
 };
